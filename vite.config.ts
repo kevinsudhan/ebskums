b6d939835bcfc40ede8611f15c25c76d5690b61a
kevinsudhan <kevinsudhan31@gmail.com>
2024-12-18 00:53:38 +0530

    Fix TypeScript build errors and add less strict build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ebskums/',
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg', '**/*.gif'],
   build: {
